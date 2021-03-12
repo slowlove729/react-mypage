a5a4d9080c63b5abe7a51b50c2a2cb9c0f844e28
slowlove729 <slowlove729@naver.com>
2021-03-12 12:50:04 +0900

    logo, navbar, jumbotron

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import H_logo from './H_logo.svg'
+import { Navbar, Jumbotron, Container } from 'react-bootstrap'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Navbar bg="dark" variant="dark">
+          <Navbar.Brand href="#home">
+            <img
+              alt=""
+              src={H_logo}
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{' '}
+            Hong's DEV Blog
+          </Navbar.Brand>
+        </Navbar>
+
+        <Jumbotron fluid>
+          <Container>
+            <h1>Hong's DEV blog</h1>
+            <p>
+              리액트 개발 블로그
+            </p>
+          </Container>
+        </Jumbotron>
     </div>
   );
 }
