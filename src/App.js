fdf68c11f267fedfb7d2dd6ea32977144dd8f544
slowlove729 <slowlove729@naver.com>
2021-03-15 20:08:36 +0900

    photos

--- src/App.js
+++ src/App.js
@@ -7,10 +7,12 @@ import { BrowserRouter, Link, Route, Switch, useParams } from 'react-router-dom'
 import React, { useState } from 'react';
 
 import PostData from './PostData'
+import PhotoData from './PhotoData'
 
 function App() {
 
   let [posts, postsMod] = useState(PostData);
+  let [photos, photosMod] = useState(PhotoData);
 
   return (
     <div className="App">
@@ -31,11 +33,11 @@ function App() {
           </Route>
           <Route path="/posts">
             <CustomNav></CustomNav>
-            <CustomCard posts={posts}></CustomCard>
+            <Postcard posts={posts}></Postcard>
           </Route>
           <Route path="/photos">
             <CustomNav></CustomNav>
-            <div>photo</div>
+            <Photocard photos={photos}></Photocard>
           </Route>
         </Switch>
       </BrowserRouter>
@@ -70,20 +72,55 @@ function CustomNav(props){
 }
 
 
-function CustomCard(props){
+function Postcard(props){
 
   return(
-    <div className="container">
+    <div className="container post">
       <div className="row">
         {
           props.posts.map((post, i)=>{
             return(
               <div className="col-md-6">
                 <Card border="secondary">
-                  <Card.Header><Link to="/posts/:postid">{post.header}</Link></Card.Header>
+                  <Card.Header>
+                    <Link to="/posts/:postid">{post.header}</Link>
+                  </Card.Header>
                   <Card.Body>
-                    <Card.Title><Link to="/posts/:postid">{post.title}</Link></Card.Title>
-                    <Card.Text><Link to="/posts/:postid">{post.text}</Link></Card.Text>
+                    <Card.Title>
+                      <Link to="/posts/:postid">{post.title}</Link>
+                    </Card.Title>
+                    <Card.Text>
+                      <Link to="/posts/:postid">{post.text}</Link>
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              </div>
+            )
+          })
+        }
+      </div>
+    </div>
+  )
+}
+
+
+function Photocard(props){
+
+  return(
+    <div className="container photo">
+      <div className="row">
+        {
+          props.photos.map((photo, i)=>{
+            return(
+              <div className="col-md-6">
+                <Card border="primary">
+                  <Card.Header>
+                    <Link to="/photos/:photoid">{photo.header}</Link>
+                  </Card.Header>
+                  <Card.Body>
+                    <Link to="/photos/:photoid">
+                      <Card.Img variant="top" src={photo.src} alt={photo.title} />
+                    </Link>
                   </Card.Body>
                 </Card>
               </div>
---
+++ src/PhotoData.js
@@ -0,0 +1,38 @@
+/* eslint-disable */
+import image_01 from './image/0101.jpg'
+import image_02 from './image/0102.jpg'
+import image_03 from './image/0103.jpg'
+import image_04 from './image/0104.jpg'
+
+let folder = [image_01, image_02, image_03, image_04]
+
+export default[
+    {
+        postid : 0,
+        header : "header-01",
+        title : "title-01",
+        src: folder[0]
+    },
+
+    {
+        postid : 1,
+        header : "header-02",
+        title : "title-02",
+        src: folder[1]
+    },
+
+    {
+        postid : 2,
+        header : "header-03",
+        title : "title-03",
+        src: folder[2]
+    },
+
+    {
+        postid : 3,
+        header : "header-04",
+        title : "title-04",
+        src: folder[3]
+    },
+
+]
